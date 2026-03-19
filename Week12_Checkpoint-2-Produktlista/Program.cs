b7aec9fa01e72dac96bb0b7adad62c599ac7d419
aLexicon99 <267594691+aLexicon99@users.noreply.github.com>
2026-03-19 09:16:24 +0100

    calculation-class
    added PrintAllProducts method, and calculation class

--- Week12_Checkpoint-2-Produktlista/Program.cs
+++ Week12_Checkpoint-2-Produktlista/Program.cs
@@ -1,19 +1,10 @@
-﻿using System.ComponentModel;
-
-Console.Clear();
-List<Product> productsList = new List<Product>();
-
+﻿List<Product> productsList = new List<Product>();
 while (true)
 {
-    AskForNewProduct();
-    Console.Write("Enter a Category: ");
-    string productCategory = Console.ReadLine();
+    Message.AskForNewProduct();
+    string productCategory = Console.ReadLine() ?? String.Empty;
     
-    if (productCategory.ToLower().Trim() == "q")
-    {
-        break;
-    }
-
+    if(productCategory.ToLower().Trim() == "q") break;
     if(productCategory.ToLower().Trim().Length > 0)
     {
         Console.Write("Enter a Product Name: ");
@@ -28,62 +19,76 @@ while (true)
         {
             Product newProduct = new Product(productCategory, productName, priceAsNumber);
             productsList.Add(newProduct);
-            newProduct.ShowSuccessMessage();
+            Message.ShowSuccessMessage();
         }
     }
 }
 
 Console.WriteLine("-------------------------------------------------------");
-PrintAllProducts(productsList);
-
-void AskForNewProduct()
-{
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine("To enter a new product - follow the steps | To quit - enter: \"Q\"");
-    Console.ResetColor();
-}
-
-void PrintAllProducts(List<Product> allproducts)
-{
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Category\t\tProduct\t\t\tPrice");
-    Console.ResetColor();
-
-    int sum = 0;
-
-    foreach (Product product in allproducts)
-    {
-        Console.WriteLine($"{product.Category}\t\t\t{product.Name}\t\t\t{product.Price}");
-
-        sum += product.Price;
-    }
-    Console.WriteLine($"\n\t\t\tTotal amount:\t\t{ sum }");
-}
+Message.PrintAllProducts(productsList);
+Console.ReadKey();
 
 
 class Product
 {
-
     public string Category { get; set; }
     public string Name { get; set; }
     public int Price { get; set; }
-
-    public Product(string category)
-    {
-        Category = category;
-    }
     public Product(string category, string name, int price)
     {
         Category = category;
         Name = name;
         Price = price;
     }
+    public static int SumOfProducts(List<Product> products) {
+        Calculation Calculate = new Calculation();
+        return Calculate.SumOfAllProductPrices(products);
+    }
+}
 
-    public void ShowSuccessMessage()
+class Calculation
+{
+    public int SumOfAllProductPrices(List<Product> products)
+    {
+        int sum = 0;
+        foreach (Product product in products)
+        {
+            sum += product.Price;
+        }
+        return sum;
+    }
+}
+
+class Message
+{
+    public static void AskForNewProduct()
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("To enter a new product - follow the steps | To quit - enter: \"Q\"");
+        Console.ResetColor();
+        Console.Write("Enter a Category: ");
+    }
+
+    public static void ShowSuccessMessage()
     {
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("The product was successfully added!");
         Console.ResetColor();
         Console.WriteLine("-------------------------------------------------------");
+    }
+
+    public static void PrintAllProducts(List<Product> products)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Category\t\tProduct\t\t\tPrice");
+        Console.ResetColor();
+
+        foreach (Product product in products)
+        {
+            Console.WriteLine($"{product.Category}\t\t\t{product.Name}\t\t\t{product.Price}");
+        }
+
+        int amount = Product.SumOfProducts(products);
+        Console.WriteLine($"\n\t\t\tTotal amount:\t\t{amount}");
     }
 }
